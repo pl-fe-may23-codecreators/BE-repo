69ad05e35043e5fc0c7f29107d9d33aee3eb5ffc
Klaudia <kklis@interia.pl>
2023-10-30 17:05:03 +0100

    Create Phones table

---
+++ config/config.js
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// const fs = require('fs')
+
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+  // test: {
+  //   username: process.env.CI_DB_USERNAME,
+  //   password: process.env.CI_DB_PASSWORD,
+  //   database: process.env.CI_DB_NAME,
+  //   host: '127.0.0.1',
+  //   port: 3306,
+  //   dialect: 'mysql',
+  //   dialectOptions: {
+  //     bigNumberStrings: true
+  //   }
+  // },
+  // production: {
+  //   username: process.env.PROD_DB_USERNAME,
+  //   password: process.env.PROD_DB_PASSWORD,
+  //   database: process.env.PROD_DB_NAME,
+  //   host: process.env.PROD_DB_HOSTNAME,
+  //   port: process.env.PROD_DB_PORT,
+  //   dialect: 'mysql',
+  //   dialectOptions: {
+  //     bigNumberStrings: true,
+  //     ssl: {
+  //       ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
+  //     }
+  //   }
+  // }
+}
