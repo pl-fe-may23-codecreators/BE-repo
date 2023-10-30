c7b13764602d56580c72e72af5c3ff7a09dd8f05
Emil Owczarek <owczarekemil@gmail.com>
2023-10-30 23:04:38 +0100

    express.static

--- dist/index.js
+++ dist/index.js
@@ -19,6 +19,7 @@ const temporaryProducts_json_1 = __importDefault(require("./temporaryProducts.js
 const app = (0, express_1.default)();
 const port = 3000;
 app.use((0, cors_1.default)());
+app.use(express_1.default.static('public'));
 app.get('/', (req, res) => {
     res.send('Działa!!! :D');
 });
