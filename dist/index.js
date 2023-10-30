b6dbaeb8165ae212c8ac10ee4d10cf7bcb3aa466
Emil Owczarek <owczarekemil@gmail.com>
2023-10-30 14:30:04 +0100

    dist

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,19 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const db_1 = __importDefault(require("./db/db"));
 const cors_1 = __importDefault(require("cors"));
 const temporaryProducts_json_1 = __importDefault(require("./temporaryProducts.json"));
 const app = (0, express_1.default)();
@@ -39,6 +49,8 @@ app.get('/products', (req, res) => {
     };
     res.json(results);
 });
-app.listen(port, () => {
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+app.listen(port, () => __awaiter(void 0, void 0, void 0, function* () {
     console.log(`Now listening on port ${port}`);
-});
+    yield db_1.default.authenticate();
+}));
