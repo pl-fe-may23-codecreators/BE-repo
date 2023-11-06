6b7f9166f8c214c59f6860660ce5ceffe93be6bd
Klaudia <kklis@interia.pl>
2023-11-06 14:46:46 +0300

    Seeding phone details in database with correct id

--- migrations/20231030151755-create-phone.js
+++ migrations/20231030151755-create-phone.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('Phones', {
-      phoneId: {
+     id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
