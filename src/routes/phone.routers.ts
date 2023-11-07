3e16c08cfd6fe0fe02641eef77a35a044f28618e
Emil Owczarek <131079210+emil-owczarek@users.noreply.github.com>
2023-11-07 12:57:45 +0100

    Update phone.routers.ts
    changes in routes order

--- src/routes/phone.routers.ts
+++ src/routes/phone.routers.ts
@@ -3,8 +3,8 @@ import { phoneControllers } from '../controllers/phone.controllers';
 
 export const phoneRoutes = express.Router();
 
-phoneRoutes.get('/', phoneControllers.getAllPhones);
-phoneRoutes.get('/:phoneId', phoneControllers.getPhone);
-phoneRoutes.get('/:phoneId/recommended', phoneControllers.getRecommended);
 phoneRoutes.get('/new', phoneControllers.newPhones);
 phoneRoutes.get('/discount', phoneControllers.discountedPhones);
+phoneRoutes.get('/:phoneId', phoneControllers.getPhone);
+phoneRoutes.get('/:phoneId/recommended', phoneControllers.getRecommended);
+phoneRoutes.get('/', phoneControllers.getAllPhones);
